f126cec7503208ab4923931a3dbd0b0788fd70c0
David Strauch <dmaster18@gmail.com>
2019-09-14 15:37:03 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
     @@all
   end
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     new_appointment = Appointment.new(date, patient, self)
     new_appointment.doctor = self
     Appointment.all << new_appointment
